37dbbafb5b7d3eaf8b1786c6a95b619b5d7677fa
NALLURE <maldobai24@gmail.com>
2024-10-09 11:03:58 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+
+var express = require('express');
+var app = express();
+
+
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.use(express.static('public'));
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+function isValidDate(date) {
+  return date instanceof Date && !isNaN(date);
+}
+
+
+app.get("/api/:date?", function(req, res) {
+  let inputDate = req.params.date;
+  let date;
+
+  if (!inputDate) {
+
+    date = new Date();
+  } else if (/^\d+$/.test(inputDate)) {
+
+    date = new Date(parseInt(inputDate));
+  } else {
+
+    date = new Date(inputDate);
+  }
+
+  if (!isValidDate(date)) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+
+
+
+
+
+var listener = app.listen(process.env.PORT || 3000, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
